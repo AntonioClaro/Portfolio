7b3434c42ff068bd6a115ede33d7191e0d7735d3
AntonioClaro <56841174+AntonioClaro@users.noreply.github.com>
2025-06-30 19:55:02 -0300

    Add files via upload

---
+++ Projetos JAVA/Sistema de Transações TGID/tgid_schema.sql
@@ -0,0 +1,87 @@
+CREATE DATABASE  IF NOT EXISTS `tgid_schema` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `tgid_schema`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tgid_schema
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` int NOT NULL,
+  `nome` varchar(40) NOT NULL,
+  `empresa_cnpj` varchar(18) NOT NULL,
+  `saldo` double NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `usuario` varchar(40) NOT NULL,
+  `senha` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`,`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'José Kyle','31.568.143/0001-78',47374,'135.698.122-3','jkyle','1234'),(2,'Anderson William','31.568.143/0001-78',47500,'335.876.214-8','awilliam','1234'),(3,'André Sousa Duarte','41.785.993/0001-49',86011.2,'456.889.434-7','asousa','1234'),(4,'Marcos Natael Martins','24.891.448/0002-11',67507.5,'670.081.449-3','mnatael','1234'),(5,'Julian Nilman ','41.785.993/0001-49',61443.9,'458.323.199-4','jnilman','1234');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empresas`
+--
+
+DROP TABLE IF EXISTS `empresas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empresas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(40) NOT NULL,
+  `taxa` double NOT NULL,
+  `saldo` double NOT NULL,
+  `cnpj` varchar(18) NOT NULL,
+  `usuario` varchar(40) NOT NULL,
+  `senha` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`,`cnpj`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empresas`
+--
+
+LOCK TABLES `empresas` WRITE;
+/*!40000 ALTER TABLE `empresas` DISABLE KEYS */;
+INSERT INTO `empresas` VALUES (1,'AKLX Edificações',2.5,512766,'31.568.143/0001-78','aklx','1234'),(2,'Natasha Consultoria',4.9,158212.13999999998,'24.891.448/0002-11','ncon','1234'),(3,'Mecar Automotivos',3.11,498976.22,'41.785.993/0001-49','maut','1234');
+/*!40000 ALTER TABLE `empresas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-30 19:55:31
